2bf265c6a3e5a19472cb4d135dbe96c4363e652f
rain <3100406687@qq.com>
2022-04-09 16:29:11 +0800

    TS+Element搭建后台管理系统

---
+++ admin/src/router/index.ts
@@ -0,0 +1,33 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import MainView from '../views/MainView.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'main',
+    component: MainView,
+    children: [
+      {
+        path: '/',
+        component: HomeView,
+      },
+    ],
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+export default router
---
+++ admin/src/views/MainView.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <!-- 左侧菜单 begin -->
+        <el-menu default-active="`menu-0`"  @select="handleSelect">
+          <el-sub-menu
+            v-for="(item, index) in menuList.item"
+            :key="`menu-${index}`"
+            :index="`menu-${index}`"
+          >
+            <template #title>
+              <span>{{ item.title }}</span>
+            </template>
+            <el-menu-item
+              v-for="(subItem, subIndex) in item.items"
+              :key="`menu-item-${index}-${subIndex}`"
+              :index="subItem.path"
+              @click="handleMenuClick(subItem)"
+            >{{ subItem.title }}</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-aside>
+      <!-- 左侧菜单 end -->
+      <!-- 头部 -->
+      <el-container>
+        <el-header>nestjs-后台管理系统</el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+export default defineComponent({
+  // 已启用类型推断
+  setup() {
+    const menu = {
+      item: [
+        {
+          title: '内容管理',
+          items: [
+            {
+              title: '首页',
+              path: '/'
+            },
+            {
+              title: '课程管理',
+              path: '/courses/list'
+            },
+            {
+              title: '课时管理',
+              path: '/elpse/list'
+            }
+          ]
+        }, {
+          title: '运营管理',
+          items: [
+            {
+              title: '首页',
+              path: '/'
+            },
+            {
+              title: '用户管理',
+              path: '/users/list'
+            }
+          ]
+        }
+      ]
+    }
+    const handleMenuClick = (e: any) => {
+      console.log(e)
+    }
+    const handleSelect = () => {
+      console.log('select')
+    }
+    const menuList = reactive(menu)
+    return {
+      menuList,
+      handleMenuClick,
+      handleSelect
+    }
+  }
+})
+</script>
